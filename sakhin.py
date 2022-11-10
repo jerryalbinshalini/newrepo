3592b8295b08a19fb4dbe5ca21ed729f2421263c
jerryalbinshalini <shalinistellus@gmail.com>
2022-11-10 11:21:59 +0530

    first commit

---
+++ sakhin.py
@@ -0,0 +1,37 @@
+##i=6
+##while (i>0):
+##    j=6
+##    while (i>i):
+##        print(***,end=*)
+##              j=j=1
+##              i=i-1
+##              print()
+##i+=1
+##if(i==5):
+##    print("5 is skipped")
+##continue
+##print('number is'+str(i))
+##print('out of loop')
+##str='network = systems'
+##for letter in str:
+##    if (letter=='e'):
+##        break
+##    print ('current letter:',letter)
+##var=10
+##while var>0:
+##    print('current variable value:',var)
+##    var=var-1
+##    if var==5:
+##        break
+##    print ("good bye!")
+##str="hello world"
+##for i in str:
+##    if(i=="d"):
+##        pass
+##print("the string is :",i)
+def new():
+    print("hello from a fuction")
+new()
+    
+    
+
